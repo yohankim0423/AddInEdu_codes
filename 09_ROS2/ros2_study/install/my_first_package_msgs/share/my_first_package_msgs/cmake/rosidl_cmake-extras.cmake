4a11e87cc24c96acc6bcbf14ad3c30bfd28aadc9
yohankim0423 <yohankim0423@gmail.com>
2024-06-14 15:13:50 +0900

    Add Ros2 codes

---
+++ 09_ROS2/ros2_study/install/my_first_package_msgs/share/my_first_package_msgs/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(my_first_package_msgs_IDL_FILES "msg/CmdAndPoseVel.idl")
+set(my_first_package_msgs_INTERFACE_FILES "msg/CmdAndPoseVel.msg")
---
+++ 09_ROS2/ros2_study/src/my_first_package/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_first_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='verlo',
+    maintainer_email='yohankim0423@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_first_node = my_first_package.my_first_node:main',
+            'my_subscriber = my_first_package.my_subscriber:main',
+            'my_publisher = my_first_package.my_publisher:main',
+            'turtle_cmd_and_pose = my_first_package.turtle_cmd_and_pose:main'
+        ],
+    },
+)
