d83da46e9ae43f6d944455b199daee3c48501234
yohankim0423 <yohankim0423@gmail.com>
2024-06-17 17:53:45 +0900

    Add Ros2 codes

---
+++ 09_ROS2/ros2_study/src/my_first_package/my_first_package/my_subscriber.py
@@ -0,0 +1,31 @@
+import rclpy as rp
+from rclpy.node import Node
+
+from turtlesim.msg import Pose
+
+class TurtlesimSubscriber(Node):
+
+	def __init__(self):
+		super().__init__('turtlesim_subscriber')
+		self.subscroption = self.create_subscription(
+			Pose,
+			'/turtle1/pose',
+			self.callback,
+			10)
+		self.subscroption # prevent unused variable warning
+
+	def callback(self, msg):
+		print("X : ", msg.x, ", Y: ", msg.y)
+
+
+def main(args=None):
+		rp.init(args=args)
+
+		turtlesim_subscriber = TurtlesimSubscriber()
+		rp.spin(turtlesim_subscriber)
+
+		turtlesim_subscriber.destroy_node()
+		rp.shutdown()
+
+if __name__ == '__main__':
+	main()
