4278f434bbd2d8942ed6a285c546bf89ed2edc37
yohankim0423 <yohankim0423@gmail.com>
2024-06-17 17:51:11 +0900

    restore

--- 09_ROS2/ros2_study/install/my_first_package_msgs/share/my_first_package_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(my_first_package_msgs_IDL_FILES "msg/CmdAndPoseVel.idl")
-set(my_first_package_msgs_INTERFACE_FILES "msg/CmdAndPoseVel.msg")
